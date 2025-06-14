73f3a57da9bcdf800fbf4754f55b206631fa8346
JRC2004Millos <julianrodriguezc@javeriana.edu.co>
2025-06-13 20:48:01 -0500

    Catálogo Responsive y Productos Añadidos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, Header, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'web-miyuki';
