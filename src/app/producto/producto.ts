efb553f4d37808eec742e1fd377db1f086e2297e
JRC2004Millos <julianrodriguezc@javeriana.edu.co>
2025-06-14 14:11:04 -0500

    Detalle de producto con link a WhatsApp para compra

--- src/app/producto/producto.ts
+++ src/app/producto/producto.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { productos } from '../catalogo/catalogo';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-producto',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './producto.html',
-  styleUrl: './producto.css'
+  styleUrl: './producto.css',
 })
 export class Producto {
+  producto: any;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.producto = productos[id];
+  }
+
+  obtenerLinkWhatsApp(): string {
+    const mensaje = `Hola, estoy interesado en el producto: ${
+      this.producto.nombre
+    } (${this.producto.tipo}) de la colección ${
+      this.producto.coleccion ?? 'general'
+    }`;
+    return `https://wa.me/573185289607?text=${encodeURIComponent(mensaje)}`;
+  }
 }
